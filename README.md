999ec9712a13402d893d221d52ddab2042eb1c06
Stanley Lowres <slowres@gmail.com>
2022-08-04 10:18:01 +0100

    Restored documents

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # odin-library
-Odin Project Library Learning project
+Learning library project
+
+
+this is a bit jank rn, but I don't fancy rewriting it a second time. Will come back to this later!
+
+Lessons learned:
+    -   CSS: less is more! Think carefully about classes and sub-classes. Assign colour variables. Organise CSS doc by section - e.g header, main, objects e.g buttons, modals
+    -   javascript: functions functions functions! How can something be a function. Assign global variables for items already in the html. Think about naming conventions! Use objects and classes. Put stuff into arrays/libraries before displaying it on the page!
+
+    
---
+++ script.js
@@ -0,0 +1,106 @@
+let library = [];
+const container = document.querySelector('.library');
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+function addBooktoLibrary() {
+
+    const popup = document.createElement('div');
+    popup.classList.add('modal');
+    
+    const exitButton = document.createElement('button');
+    exitButton.textContent = 'Exit';
+    exitButton.addEventListener('click', () => {
+        container.removeChild(popup);
+    })
+
+    const form = document.createElement('div');
+    
+    const title = document.createElement('input');
+
+
+    const submitButton = document.createElement('button');
+    submitButton.textContent = 'Submit';
+    submitButton.addEventListener('click', () => {
+        const newBook = new Book(title.value,author='test',pages='test',read='test');
+        library.push(newBook);
+        console.log(library);
+
+        renderBook(newBook);
+        container.removeChild(popup);
+    })
+
+    popup.appendChild(exitButton);
+    popup.appendChild(title);
+    popup.appendChild(submitButton);
+
+    container.appendChild(popup);
+
+    //lets make this a modal - first - CSS
+    /*let title = prompt('Title');
+    let author = prompt('Author');
+    let pages = prompt('No. of pages');
+    let read = prompt('Read? True/false');
+
+    const newBook = new Book(title,author,pages,read);
+    library.push(newBook);
+    console.log(library);
+
+    renderBook(newBook); */
+}
+
+function renderBook(book) {
+    //create a card for each book in the library with the corresponding info.
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const title = document.createElement('h1');
+    title.textContent = book.title;
+
+    const author = document.createElement('h2');
+    author.textContent = book.author;
+
+    const pages = document.createElement('p');
+    pages.textContent = `${book.pages} pages`;
+
+    const read = document.createElement('p');
+    read.textContent = book.read;
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(read);
+
+    container.appendChild(card);
+}
+
+function refreshLibrary() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    library.forEach(book => {
+        renderBook(book);
+    })
+}
+
+const addBookButton = document.querySelector('.add-book');
+addBookButton.addEventListener('click', () => {
+    addBooktoLibrary();
+})
+
+const refreshButton = document.querySelector('.refresh');
+refreshButton.addEventListener('click', () => {
+    refreshLibrary();
+})
+
+
+const moby = new Book('Moby Dick','Herman Melville',600,false);
+const pride = new Book('Pride and Prejudice','Jane Austen',750,false);
+
+library.push(moby);
+library.push(pride);
